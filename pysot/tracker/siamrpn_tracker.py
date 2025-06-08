c4270920fa9c724a63cdcb575ea1148e18c6dae6
eigenvectorBazuz <felix.goldberg@gmail.com>
2025-06-08 16:55:46 +0300

    Update siamrpn_tracker.py

--- pysot/tracker/siamrpn_tracker.py
+++ pysot/tracker/siamrpn_tracker.py
@@ -241,6 +241,7 @@ class SiamRPNTracker(SiameseTracker):
     #             width,
     #             height]
     #     best_score = score[best_idx]
+          print('new')
     #     return {
     #             'bbox': bbox,
     #             'best_score': best_score,
